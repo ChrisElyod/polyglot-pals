ce457b1146ffba7d1970ea9900ad5145c5481d8c
ChrisElyod <sean.chris.doyle@gmail.com>
2021-10-25 12:17:49 -0400

    Remove server directory with too much playing around, to be redeveloped

--- server/src/lf-penpal/lf-penpal.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { LfPenpalService } from './lf-penpal.service';
-import { LfPenpalController } from './lf-penpal.controller';
-
-@Module({
-  controllers: [LfPenpalController],
-  providers: [LfPenpalService]
-})
-export class LfPenpalModule {}
